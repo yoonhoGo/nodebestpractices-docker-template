47d16a55734b0687b7cd1344a00098d557b86e81
Yoonho Go <lylisha@gmail.com>
2020-11-30 22:15:26 +0900

    feat: ProcessManager

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import {bootstrap} from './app';
 import {PORT, HOSTNAME} from './config/environments';
+import pm, {ProcessManager} from './config';
 
-bootstrap(PORT, HOSTNAME).catch(() => {});
+bootstrap(PORT, HOSTNAME)
+  .then(server => pm.onGracefulShutdown(server))
+  .catch(err => {
+    console.error('Bootstrap starting error:', err);
+
+    ProcessManager.hangUp();
+  });
